9945b76cfeb6b0038429010c2ed9394dd74afd53
Raghav Narang <raghav.narang.con@hotstar.com>
2026-05-17 15:13:29 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length != t.length) {
+            return false;
+        }
+        s = s.toLowerCase()
+        t = t.toLowerCase()
+        const map = new Map()
+        for(let i = 0; i<s.length;i++) {
+            map.set(s[i], (map.get(s[i]) || 0) + 1)
+            map.set(t[i], (map.get(t[i]) || 0) - 1)
+        }
+console.log(map)
+        return map.values().every(v=>v===0)
+    }
+}
